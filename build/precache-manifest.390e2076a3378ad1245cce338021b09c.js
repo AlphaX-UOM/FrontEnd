232363c4a2ddf70ec35b0888e90732ef69425cd5
$udam <48345668+gdsghost@users.noreply.github.com>
2020-12-31 17:39:15 +0530

    Add files via upload

---
+++ build/precache-manifest.390e2076a3378ad1245cce338021b09c.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb1a4d3e2daf55dd64649c5a5974be5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2612e04058c8b7e9ba02",
+    "url": "/static/css/2.2d23c495.chunk.css"
+  },
+  {
+    "revision": "8f3e4c2e1de21effa69e",
+    "url": "/static/css/main.6c01d496.chunk.css"
+  },
+  {
+    "revision": "2612e04058c8b7e9ba02",
+    "url": "/static/js/2.2bbad6d2.chunk.js"
+  },
+  {
+    "revision": "14b1f7e9e2ea84b0cb837970009b4512",
+    "url": "/static/js/2.2bbad6d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f3e4c2e1de21effa69e",
+    "url": "/static/js/main.16f4c62c.chunk.js"
+  },
+  {
+    "revision": "12d6586a204116219286",
+    "url": "/static/js/runtime-main.d35b5f43.js"
+  },
+  {
+    "revision": "05d19e21d0b0555a8caffe68cc9aeeae",
+    "url": "/static/media/Bus.05d19e21.jfif"
+  },
+  {
+    "revision": "59bcae022d32a31b855dc4b1086c9058",
+    "url": "/static/media/Intermediate.59bcae02.jpg"
+  },
+  {
+    "revision": "1e76f4702bb60f2a40e622ebff5aec3a",
+    "url": "/static/media/background.1e76f470.jpg"
+  },
+  {
+    "revision": "4c9996d8284a10f2f5a921f770c6dced",
+    "url": "/static/media/banner.4c9996d8.jpg"
+  },
+  {
+    "revision": "e5bf0df28301e6aee53447b4e22252be",
+    "url": "/static/media/estate.e5bf0df2.jpg"
+  },
+  {
+    "revision": "1fac4241493403edb55b80a69d733d14",
+    "url": "/static/media/event.1fac4241.jfif"
+  },
+  {
+    "revision": "476ab3c78e6c3bb4bf619fd24d9de14f",
+    "url": "/static/media/guide (2).476ab3c7.jfif"
+  },
+  {
+    "revision": "35545383d227f791513a329deac2ab1d",
+    "url": "/static/media/hotel.35545383.jfif"
+  },
+  {
+    "revision": "e609615402e278bfff43746e5463223c",
+    "url": "/static/media/img-2.e6096154.svg"
+  },
+  {
+    "revision": "f083ce5f719ab750cf6a1c03df4bdd28",
+    "url": "/static/media/india.f083ce5f.jpg"
+  },
+  {
+    "revision": "85c79d2c642d8b482321b64417384f3b",
+    "url": "/static/media/suv.85c79d2c.jfif"
+  },
+  {
+    "revision": "9ca38dfb4574083bd36d453c8767e8bf",
+    "url": "/static/media/v.9ca38dfb.jpg"
+  },
+  {
+    "revision": "a2aada8111abd7b10caac9c44b828f4b",
+    "url": "/static/media/vehicle.a2aada81.jpg"
+  }
+]);
