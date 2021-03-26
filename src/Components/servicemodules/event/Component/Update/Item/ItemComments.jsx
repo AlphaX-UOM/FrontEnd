eb07d03f0f37eab5e56123e65dbb17231689f492
$udam <48345668+gdsghost@users.noreply.github.com>
2021-03-27 01:08:22 +0530

    event comment

--- src/Components/servicemodules/event/Component/Update/Item/ItemComments.jsx
+++ src/Components/servicemodules/event/Component/Update/Item/ItemComments.jsx
@@ -1,82 +1,150 @@
 import React, { useEffect, useState } from "react";
 import "./ItemComments.css";
+import Rating from "@material-ui/lab/Rating";
+import TextField from "@material-ui/core/TextField";
+import Box from "@material-ui/core/Box";
+import Comment from "./ItemOneComment";
+import { connect } from "react-redux";
 import axios from "axios";
 
 const Comments = (props) => {
-  
-    const [commentList, setCommentList] = useState();
+  const [commentList, setCommentList] = useState();
+  const [value, setValue] = React.useState(2);
+  const [comment, setComment] = useState();
+  const [ordered, setOrdered] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-    useEffect(() => {
-        fetch(
-            'https://alphax-api.azurewebsites.net/api/eventplannerservices/GetEventDetails/' +
-            props.add_id
-        )
-            .then((res) => res.json())
-            .then((data) => {
-                setCommentList(data);
-            });
-    }, []);
+  const handleFormData = () => {
+    // comment post request goes here
+    let commentObject = {
+      createdAt: new Date(),
+      rating: value,
+      content: comment,
+      userID: props.userCred.id,
+      eventPlannerServiceID: props.add_id,
+    };
 
+    axios
+      .post(
+        "https://alphax-api.azurewebsites.net/api/eventplannerservicecomments",
+        commentObject
+      )
+      .then(function(response) {
+        console.log(response);
+        setLoading(true);
+      });
+  };
 
-    const commentListComponent = () => {
-        console.log(commentList.eventPlannerServiceComments);
-        return commentList.eventPlannerServiceComments.map((commentListObject) => {
-          return (
-            <div className="col-md-9">
-                  <div className="media g-mb-30 media-comment">
-                    <div className="media-body u-shadow-v18 g-bg-secondary g-pa-30">
-                      <div className="g-mb-15">
-                        <h5 className="h5 g-color-gray-dark-v1 mb-0 txtcolorx">
-                          {commentListObject.user.firstName} {commentListObject.user.lastName}
-                        </h5>
+  const handleInputComment = (event) => {
+    // setting the comment description
+    setComment(event.target.value);
+    console.log("comment -> " + event.target.value);
+  };
 
-                        <span className="g-color-gray-dark-v4 g-font-size-12">
-                          {commentListObject.createdAt}
-                        </span>
+  const handleInputRating = (event) => {
+    // setting the rating value
+    setValue(event.target.value);
+    console.log("rating -> " + event.target.value);
+  };
 
-                        <ul className="list-inline d-sm-flex my-0">
-                          <li className="list-inline-item ml-auto"></li>
-                        </ul>
-                        <hr />
-                      </div>
+  useEffect(() => {
+    fetch(
+      "https://alphax-api.azurewebsites.net/api/eventplannerservices/GetEventDetails/" +
+        props.add_id
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setCommentList(data);
+        setLoading(false);
+      });
+  }, [loading]);
 
-                      <p>
-                        {commentListObject.content}
-                      </p>
+  useEffect(() => {
+    fetch(
+      "https://alphax-api.azurewebsites.net/api/eventplannerservicereservations"
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        
+        if (props.userCred.id !== undefined) {
+          console.log("user -> "+props.userCred.id);
+          data = data.filter( (item) => item.userID === props.userCred.id && item.eventPlannerServiceID === props.add_id );
 
-                      <hr className="" />
+          if (data[0] !== undefined) {
+            setOrdered(true);
+          } else {
+            setOrdered(false);
+          }
+        }
+      });
+  }, []);
 
-                      <ul className="list-inline d-sm-flex my-0">
-                        <li className="list-inline-item g-mr-20">
-                          <a
-                            className="u-link-v5 g-color-gray-dark-v4 g-color-primary--hover"
-                            href="#!"
-                          >
-                            <i className="fa fa-thumbs-up g-pos-rel g-top-1 g-mr-3 txtcolorx "></i>
-                            178
-                          </a>
-                        </li>
-                        <li className="list-inline-item g-mr-20">
-                          <a
-                            className="u-link-v5 g-color-gray-dark-v4 g-color-primary--hover"
-                            href="#!"
-                          >
-                            <i className="fa fa-thumbs-down g-pos-rel g-top-1 g-mr-3 txtcolorx"></i>
-                            34
-                          </a>
-                        </li>
-                      </ul>
-                    </div>
-                  </div>
-                </div>
-          );
-        });
-      };
+  const commentListComponent = () => {
+    console.log(commentList.eventPlannerServiceComments);
+    return commentList.eventPlannerServiceComments.map((commentListObject) => {
+      return <Comment data={commentListObject} />;
+    });
+  };
 
+  const commentInput = () => {
+    return (
+      <div className="col-md-9">
+        <div className="media g-mb-30 media-comment">
+          <div className="media-body u-shadow-v18 g-bg-secondary g-pa-30">
+            <div className="g-mb-15">
+              <h5 className="h5 g-color-gray-dark-v1 mb-0 txtcolorx">
+                {props.userCred.firstName} {props.userCred.lastName}
+              </h5>
 
-      if(commentList===undefined){
-       return  <p>Loading....</p>
-      }
+              <span className="g-color-gray-dark-v4 g-font-size-12">
+                {new Date().toISOString().slice(0, 10)}
+              </span>
+
+              <ul className="list-inline d-sm-flex my-0">
+                <li className="list-inline-item ml-auto"></li>
+              </ul>
+              <hr />
+            </div>
+
+            <TextField
+              id="standard-basic"
+              fullWidth
+              label="Input your comment"
+              onChange={handleInputComment}
+            />
+
+            <hr className="" />
+
+            <ul className="list-inline d-sm-flex my-0">
+              <li className="list-inline-item g-mr-20">
+                <Rating
+                  name="pristine"
+                  value={value}
+                  onChange={handleInputRating}
+                />
+              </li>
+              <li className="list-inline-item g-mr-20"></li>
+              <li className="list-inline-item g-mr-20"></li>
+              <li className="list-inline-item g-mr-20"></li>
+              <li className="list-inline-item g-mr-20">
+                <button
+                  type="button"
+                  class="btn btn-outline-success"
+                  onClick={handleFormData}
+                >
+                  Submit
+                </button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  if (commentList === undefined) {
+    return <p>Loading....</p>;
+  }
 
   return (
     <div>
@@ -191,10 +259,10 @@ const Comments = (props) => {
                 </div>
 
                 {/* another comment starts */}
-                
+
                 {commentListComponent()}
                 {/* comment end here */}
-
+                {props.userCred.id!==undefined && ordered ? commentInput() : <p>login to comment</p> }
               </div>
             </div>
           </div>
@@ -205,4 +273,10 @@ const Comments = (props) => {
   );
 };
 
-export default Comments;
+const mapStateToProps = (state) => {
+  return {
+    userCred: state.eventpnl.userCred,
+  };
+};
+
+export default connect(mapStateToProps)(Comments);
